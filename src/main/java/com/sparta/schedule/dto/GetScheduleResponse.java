1dd9702941e9fac8922062436408fb1c258bd3ed
B-ChJ <bchaejoo@gmail.com>
2025-10-17 16:44:08 +0900

    ✅ 데이터 조회 시 id가 보이지 않는 이슈 해결

--- src/main/java/com/sparta/schedule/dto/GetScheduleResponse.java
+++ src/main/java/com/sparta/schedule/dto/GetScheduleResponse.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 @Getter
 public class GetScheduleResponse {
 
+    private final Long id;
     private final String title;
     private final String content;
     private final String registrant;
@@ -15,6 +16,7 @@ public class GetScheduleResponse {
 
     public GetScheduleResponse(Long id, String title, String content, String registrant,
                                LocalDateTime createdAt, LocalDateTime modifiedAt) {
+        this.id = id;
         this.title = title;
         this.content = content;
         this.registrant = registrant;
